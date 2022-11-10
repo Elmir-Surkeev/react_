e5cc439605f42f52958c941102146ce5a75950d0
Elmir <elmirsurkeev@gmail.com>
2022-11-10 16:02:48 +0600

    about front and back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
+//import ClassCounter from "./components/ClassCounter";
 //import Counter from "./components/counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/app.css";
 function App() {
-  //  const [value, setValue] = useState("text in input");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "description" },
+    { id: 2, title: "Dart", body: "description" },
+    { id: 3, title: "GO", body: "description" },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Java", body: "description" },
+    { id: 2, title: "Python", body: "description" },
+    { id: 3, title: "C++", body: "description" },
+  ]);
+
   return (
     <div className="App">
-      <ClassCounter />
+      <PostList posts={posts} title="Фронтенд языки" />
+      <PostList posts={posts2} title="Бэкенд языки" />
     </div>
   );
 }
